fdccdcb058b5d0a3fe313bfd0562af3e8f93a8b3
Augusto Guerra <augustouguerra@gmail.com>
2022-11-01 08:28:08 -0300

    Removed node_modules

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { crearArchivo } = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
 
-console.log(argv);
+// console.log(argv);
 console.log('base: yargs', argv.base);
 
 // Array destructoring 
