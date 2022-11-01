9e3abb0539e3d0b2e2f2f48412b41a243e6c59ee
Augusto Guerra <augustouguerra@gmail.com>
2022-11-01 08:05:51 -0300

    clean

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const fs = require('fs');
 const { crearArchivo } = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
 
-
-
-
-// console.log(process.argv);
 console.log(argv);
 console.log('base: yargs', argv.base);
 
